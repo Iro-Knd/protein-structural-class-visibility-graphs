fff3bd32198c2e55930920389ec2225ec3f3db7e
Iro-Knd <70773887+Iro-Knd@users.noreply.github.com>
2026-09-17 01:22:54 +0300

    Horizontal Visibility Graph 2-Dimensional

---
+++ HVG2D/HVG2D_OR_all.py
@@ -0,0 +1,32 @@
+import networkx as nx
+
+def horizontal_visibility_2D_or(series):
+    n = len(series)
+    g = nx.Graph()
+
+    # nodes
+    for i, vec in enumerate(series, start=1):
+        g.add_node(i, mag=tuple(vec))
+
+    # neighbor edges always
+    for i in range(1, n):
+        g.add_edge(i, i + 1)
+
+    # HVG OR visibility checks
+    for i in range(1, n + 1):
+        xi, yi = series[i - 1]
+        for j in range(i + 2, n + 1):
+            xj, yj = series[j - 1]
+
+            min_x = min(xi, xj)
+            min_y = min(yi, yj)
+
+            connect = all(
+                (series[k - 1][0] < min_x) or (series[k - 1][1] < min_y)
+                for k in range(i + 1, j)
+            )
+
+            if connect:
+                g.add_edge(i, j)
+
+    return g
